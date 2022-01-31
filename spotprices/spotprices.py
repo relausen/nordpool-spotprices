ace82ba8310aadd21346b54f3edaf64b3b67c5d9
Rune Lausen <rune@lausennet.dk>
2022-01-31 13:43:44 +0100

    Separator between today and tomorrow. Mark min and max price per day.

--- spotprices/spotprices.py
+++ spotprices/spotprices.py
@@ -6,6 +6,27 @@ import requests
 from colorama import Fore, Style
 
 
+def print_prises_for(prices):
+    min_price = min(prices, key=lambda p: p['SpotPriceEUR'])['SpotPriceEUR']
+    max_price = max(prices, key=lambda p: p['SpotPriceEUR'])['SpotPriceEUR']
+
+    colorama.init()
+    for price_data in prices:
+        price_time = datetime.fromisoformat(price_data['HourDK'])
+        eur_price = price_data['SpotPriceEUR']
+        dkk_price_entry = price_data['SpotPriceDKK']
+        dkk_price = round(dkk_price_entry/10) if dkk_price_entry else round(eur_price*eur_dkk/10)
+        estimate_suffix = '' if dkk_price_entry else '  *'
+        min_max_suffix = ''
+        if eur_price == min_price:
+            min_max_suffix = ' (min)'
+        elif eur_price == max_price:
+            min_max_suffix = ' (max)'
+        fg_color = Fore.GREEN if price_time.date() == datetime.now().date() and price_time.hour == datetime.now().hour else ''
+        print(f'{fg_color}{price_time} {dkk_price:>4}{min_max_suffix}{estimate_suffix}{Style.RESET_ALL}')
+    colorama.deinit()
+
+
 headers = {
     'content-type': 'application/json',
 }
@@ -42,15 +63,11 @@ usd_eur = exchange_rates['rates']['EUR']
 usd_dkk = exchange_rates['rates']['DKK']
 eur_dkk = usd_dkk/usd_eur
 
-colorama.init()
+all_prices = prices_request.json()['data']['elspotprices']
+prices_today = [price_data for price_data in all_prices if datetime.fromisoformat(price_data['HourDK']).date() == datetime.now().date()]
+prices_tomorrow = [price_data for price_data in all_prices if price_data not in prices_today]
 
-for price_data in prices_request.json()['data']['elspotprices']:
-    price_time = datetime.fromisoformat(price_data['HourDK'])
-    eur_price = price_data['SpotPriceEUR']
-    dkk_price_entry = price_data['SpotPriceDKK']
-    dkk_price = round(dkk_price_entry/10) if dkk_price_entry else round(eur_price*eur_dkk/10)
-    suffix = '' if dkk_price_entry else '  *'
-    fg_color = Fore.GREEN if price_time.date() == datetime.now().date() and price_time.hour == datetime.now().hour else ''
-    print(f'{fg_color}{price_time} {dkk_price:>4}{suffix}{Style.RESET_ALL}')
-
-colorama.deinit()
+print_prises_for(prices_today)
+if prices_tomorrow:
+    print('-' * 24)
+    print_prises_for(prices_tomorrow)
