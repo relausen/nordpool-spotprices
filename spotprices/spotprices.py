23437502e64dc9c4704b8fb33c3636fb7781c73f
Rune Lausen <rune@lausennet.dk>
2022-02-04 12:51:13 +0100

    Better formatting for min and max rows

--- spotprices/spotprices.py
+++ spotprices/spotprices.py
@@ -22,7 +22,7 @@ def print_prises_for(prices, now_color=Fore.GREEN):
         elif eur_price == max_price:
             min_max_suffix = ' (max)'
         fg_color = now_color if price_time.date() == datetime.now().date() and price_time.hour == datetime.now().hour else ''
-        print(f'{fg_color}{price_time} {dkk_price:>4}{min_max_suffix}{estimate_suffix}')
+        print(f'{fg_color}{price_time} {dkk_price:>4}{estimate_suffix}{min_max_suffix}')
         # print(f'{fg_color}{price_time} {dkk_price:>4}{min_max_suffix}{estimate_suffix}{Style.RESET_ALL}')
 
 
